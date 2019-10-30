8d779dd25a4567c6d2c938ff50880fecfb9578d7
Jose C Gomez <jose@josecgomez.com>
2019-10-29 21:50:57 -0400

    Add project files.

---
+++ ThreadedServiceSample/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ThreadedServiceSample
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.svcProcInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.svcSampleInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // svcProcInstaller
+            // 
+            this.svcProcInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.svcProcInstaller.Password = null;
+            this.svcProcInstaller.Username = null;
+            // 
+            // svcSampleInstaller
+            // 
+            this.svcSampleInstaller.Description = "Sample Service";
+            this.svcSampleInstaller.DisplayName = "Sample Service";
+            this.svcSampleInstaller.ServiceName = "svcSample";
+            this.svcSampleInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.svcProcInstaller,
+            this.svcSampleInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller svcProcInstaller;
+        private System.ServiceProcess.ServiceInstaller svcSampleInstaller;
+    }
+}
---
+++ ThreadedServiceSample/svcSample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadedServiceSample
+{
+    public partial class svcSample : ServiceBase
+    {
+        private bool running = false;
+        Thread thWorker = null;
+        public svcSample()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            running = true;
+            thWorker = new Thread(DoWork);
+            thWorker.Start();
+        }
+
+        protected override void OnStop()
+        {
+            running = false;
+        }
+
+        private void DoWork()
+        {
+            while(running)
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry("Service Loop", EventLogEntryType.Information, 101, 1);
+                }
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
